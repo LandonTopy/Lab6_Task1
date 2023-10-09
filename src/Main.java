8e7ec13801c393d29ff0b1b7cdccc2773ee9674c
LandonTopy <landontopy@gmail.com>
2023-10-09 09:31:17 -0400

    Hi

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        //Variables
         double degreesC;
         double degreesF;
         String yn;
